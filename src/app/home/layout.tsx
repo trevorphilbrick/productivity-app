ab5467fad397cdc4f9244d87949c05c2aed16266
Trevor <trevorphilbrick@outlook.com>
2024-02-05 13:52:19 -0800

    added rough layout and page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Weather from "@/components/ui/home/weather";
 import Events from "@/components/ui/home/events";
+import { Sheet } from "@/components/ui/sheet";
+import AddTaskForm from "@/components/ui/home/addTaskForm";
 
 function layout({
   children,
@@ -8,14 +10,17 @@ function layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-screen  md:flex md:flex-col md:items-center ">
-      <div className="bg-blue-500 w-screen">nav</div>
-      <div className="flex flex-col items-center md:items-start md:flex-row md:w-11/12 lg:4/5 max-w-6xl mt-6">
-        <Events />
-        <div style={{ flex: 1 }}>{children}</div>
-        <Weather />
+    <Sheet>
+      <div className="w-screen  md:flex md:flex-col md:items-center ">
+        <div className="bg-blue-500 w-screen">nav</div>
+        <div className="flex flex-col items-center md:items-start md:flex-row md:w-11/12 lg:4/5 max-w-6xl mt-6">
+          <Events />
+          <div className="flex-1 px-4">{children}</div>
+          <Weather />
+        </div>
       </div>
-    </div>
+      <AddTaskForm />
+    </Sheet>
   );
 }
 
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,131 @@
+import TaskCard from "@/components/ui/home/taskCard";
+import { RxPlus } from "react-icons/rx";
+import { SheetTrigger } from "@/components/ui/sheet";
+
+type Todo = {
+  title: string;
+  description: string;
+  status: "Pending" | "In Progress" | "Completed";
+};
+
+const todoData: Todo[] = [
+  {
+    title: "Complete project proposal draft",
+    description:
+      "Finish the first draft of the project proposal for the upcoming client project.",
+    status: "In Progress",
+  },
+  {
+    title: "Review team's weekly progress reports",
+    description:
+      "Go through the weekly reports submitted by the team to assess progress and identify any issues.",
+    status: "Completed",
+  },
+  {
+    title: "Prepare slides for Monday's presentation",
+    description:
+      "Create a PowerPoint presentation for Monday's meeting with stakeholders.",
+    status: "In Progress",
+  },
+  {
+    title: "Update project timeline and milestones",
+    description:
+      "Revise the project timeline based on the latest progress and set new milestones.",
+    status: "In Progress",
+  },
+  {
+    title: "Call mom to check in",
+    description:
+      "Make a phone call to mom to catch up and see how she's doing.",
+    status: "Pending",
+  },
+  {
+    title: "Book dentist appointment for next month",
+    description: "Schedule a dental check-up for next month.",
+    status: "Pending",
+  },
+  {
+    title: "Sort through old clothes for donation",
+    description:
+      "Organize old clothes to decide what to keep and what to donate.",
+    status: "Completed",
+  },
+  {
+    title: "Plan weekend trip with friends",
+    description: "Coordinate with friends to plan a weekend getaway.",
+    status: "Completed",
+  },
+  {
+    title: "Sign up for the local 5K run",
+    description:
+      "Register for the 5K run happening in the local community next month.",
+    status: "Pending",
+  },
+  {
+    title: "Meal prep for the week",
+    description:
+      "Prepare meals in advance for the upcoming week to ensure healthy eating.",
+    status: "Pending",
+  },
+  {
+    title: "Schedule yearly physical examination",
+    description: "Book an appointment for the annual physical check-up.",
+    status: "Pending",
+  },
+  {
+    title: "Research new yoga classes to join",
+    description: "Look for new yoga classes in the area to join for fitness.",
+    status: "Pending",
+  },
+  {
+    title: "Finish reading 'Creative Confidence'",
+    description:
+      "Complete reading the book 'Creative Confidence' to enhance creativity skills.",
+    status: "Pending",
+  },
+  {
+    title: "Enroll in online course on data analysis",
+    description:
+      "Sign up for an online course to improve data analysis skills.",
+    status: "Pending",
+  },
+  {
+    title: "Watch tutorial on advanced Excel functions",
+    description: "Watch a video tutorial to learn advanced functions in Excel.",
+    status: "Pending",
+  },
+  {
+    title: "Practice coding for 30 minutes daily",
+    description:
+      "Dedicate 30 minutes every day to practice coding and improve programming skills.",
+    status: "Pending",
+  },
+];
+
 function Page() {
-  return <div>Page</div>;
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <h1 className="font-semibold text-lg mb-4">Tasks</h1>
+        <SheetTrigger className="flex items-center text-sm">
+          add
+          <RxPlus className="ml-1" />
+        </SheetTrigger>
+      </div>
+      {todoData.map(
+        (
+          todo: {
+            title: string;
+            description: string;
+            status: "Pending" | "In Progress" | "Completed";
+          },
+          index: number
+        ) => {
+          return <TaskCard todo={todo} key={index} />;
+        }
+      )}
+    </>
+  );
 }
 
 export default Page;
