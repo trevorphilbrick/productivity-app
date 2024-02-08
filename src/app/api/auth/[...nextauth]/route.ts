9e06071469a807dfaa011232e15ed1993a9e1e46
Trevor <trevorphilbrick@outlook.com>
2024-02-07 23:12:42 -0800

    fixed ts errors for possibly undefined values

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GithubProvider from "next-auth/providers/github";
 export const authOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET,
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID ?? "",
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET ?? "",
     }),
   ],
 };
