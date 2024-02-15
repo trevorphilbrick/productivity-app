11208760fc1efb1bf0a6648ed29e38bcd0153961
Trevor <trevorphilbrick@outlook.com>
2024-02-15 12:30:59 -0800

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-import withPWA from "next-pwa";
-
 const nextConfig = {
   images: {
     domains: ["openweathermap.org"],
   },
 };
 
-export default withPWA();
+export default nextConfig;
