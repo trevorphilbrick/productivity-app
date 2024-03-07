4b00e7bf6ccba91a4b027e865cba3bbe2298e98b
Trevor <trevorphilbrick@outlook.com>
2024-03-06 21:40:02 -0800

    updated type error

--- src/context/sessionContext.tsx
+++ src/context/sessionContext.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { createContext, useContext } from "react";
-import { validateRequest } from "@/app/lib/auth";
+import { validateRequest } from "@/lib/auth";
 type ContextType = Awaited<ReturnType<typeof validateRequest>>;
 const SessionContext = createContext<ContextType>({
   session: null,
