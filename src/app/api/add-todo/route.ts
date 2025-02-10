69542a3c8d05c0b46def7b31beac82a14419131b
Trevor <trevorphilbrick@outlook.com>
2025-02-10 15:00:47 -0800

    make description optional on task card

--- src/app/api/add-todo/route.ts
+++ src/app/api/add-todo/route.ts
@@ -14,9 +14,11 @@ export async function GET(request: Request) {
     if (!user) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-    if (!title || !description || !status || !priority)
+    if (!title || !status || !priority)
       throw new Error("Missing required fields.");
-    await sql`INSERT INTO todos (Title, Description, Status, Priority, UserId) VALUES (${title}, ${description}, ${status}, ${priority}, ${user.username});`;
+    await sql`INSERT INTO todos (Title, Description, Status, Priority, UserId) VALUES (${title}, ${
+      description || null
+    }, ${status}, ${priority}, ${user.username});`;
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
@@ -24,3 +26,24 @@ export async function GET(request: Request) {
   const tasks = await sql`SELECT * FROM todos;`;
   return NextResponse.json({ tasks }, { status: 200 });
 }
+
+// [
+//   {
+//     "title": "Allow No Description On Tasks",
+//     "description": "Currently, the add task section requires a description. Make it possible to leave blank. ",
+//     "status": "Pending",
+//     "priority": "low",
+//     "id": 60,
+//     "user_id": null,
+//     "userid": "trevorphilbrick"
+//   },
+//   {
+//     "title": "Change sheet appearance ",
+//     "description": "Update sheet appearance to match the new color scheme/ design",
+//     "status": "Pending",
+//     "priority": "high",
+//     "id": 74,
+//     "user_id": null,
+//     "userid": "trevorphilbrick"
+//   }
+// ]
