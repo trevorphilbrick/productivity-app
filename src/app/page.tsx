1b419f147213b47cf4ef3a44676b2490a0ead7f5
Trevor <trevorphilbrick@outlook.com>
2025-02-15 20:47:15 -0800

    Landing page hero redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import blackMobile from "../../public/mobile-black.png";
-import blackDesktop from "../../public/3-devices-black.png";
 import { validateRequest } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
@@ -11,29 +7,17 @@ export default async function Home() {
     redirect("/dashboard");
   }
   return (
-    <main className="flex flex-col items-center text-wrap">
-      {/* <div className="flex flex-col md:flex-row w-full md:mx-8 max-w-screen-xl px-4 ">
-        <div className=" mb-4 md:w-96 md:mb-0 ">
-          <h1 className=" text-5xl md:text-6xl mb-4">
-            Unlock Your Productivity Powerhouse!
-          </h1>
-          <p className="text-slate-600 mb-8">
-            From jotting down spontaneous ideas to managing complex projects,
-            this is your go-to for staying organized and ahead of the curve.
-          </p>
-          <Button className="w-full">Join Now</Button>
-        </div>
-        <Image src={blackMobile} alt="black mobile" className="md:hidden" />
-        <Image
-          src={blackDesktop}
-          alt="black desktop"
-          className="hidden md:block"
-        />
-      </div> */}
-      <div className="bg-[url(https://5ftd8mwqickudvmi.public.blob.vercel-storage.com/Gradient-4kKNDqrDamDqgIp47BgNd54TLgJV9T.png)] bg-cover bg-center bg-no-repeat  mx-8 rounded-md">
-        <h1 className=" text-5xl md:text-6xl lg:text-7xl text-center my-16 font-semibold lg:mx-24">
+    <main>
+      <div className="bg-[url(https://5ftd8mwqickudvmi.public.blob.vercel-storage.com/Gradient-4kKNDqrDamDqgIp47BgNd54TLgJV9T.png)] bg-cover bg-center bg-no-repeat rounded-md py-24 container mx-auto mb-16">
+        <h1 className=" text-5xl md:text-6xl lg:text-7xl text-center  font-semibold lg:mx-24 mb-4">
           Unlock A Productivity Powerhouse
         </h1>
+        <p className="text-center text-xl">
+          Note Taking | Task Tracking | Link Saving
+        </p>
+      </div>
+      <div className="w-full  h-64  inset-shadow bg-[url(/grid-mobile.png)] bg-cover bg-center bg-no-repeat md:bg-[url(/grid-desktop.png)]">
+        <div className="container mx-auto"></div>
       </div>
     </main>
   );
